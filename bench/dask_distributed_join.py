c99518d6e7efb6142c1025ce8667c3b406323068
vibhatha <vibhatha@gmail.com>
2021-04-18 09:51:09 -0400

    dask join v11

--- bench/dask_distributed_join.py
+++ bench/dask_distributed_join.py
@@ -40,7 +40,7 @@ def start_cluster(ips, scheduler_host, python_env, procs, nodes):
     #     ["ssh", "v-001", "/N/u2/d/dnperera/victor/git/cylon/ENV/bin/dask-scheduler", "--interface", "enp175s0f0",
     #      "--scheduler-file", "/N/u2/d/dnperera/dask-sched.json"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     subprocess.Popen(
-        ["ssh", scheduler_host, python_env + "/bin/dask-scheduler"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        ["ssh", scheduler_host, python_env + "/bin/dask-scheduler", "--scheduler-file", "/N/u2/d/vlabeyko/dask-sched.json"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
     time.sleep(5)
 
