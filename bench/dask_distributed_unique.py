facc2c1ead42cedac1f129d0d0db184f556933b3
vibhatha <vibhatha@gmail.com>
2021-04-20 09:24:29 -0400

    updated dask_dist_unique with dask-cluster setup v3

--- bench/dask_distributed_unique.py
+++ bench/dask_distributed_unique.py
@@ -100,7 +100,9 @@ def dask_drop_duplicates(scheduler_host, num_rows, base_file_path, num_nodes, pa
     client.persist([df_l])
     print("rows", len(df_l), flush=True)
     join_time = time.time()
-    out = df_l.drop_duplicates().compute()
+    out = df_l.drop_duplicates()
+    out.visualize("unique_graph.svg")
+    res = out.compute()
     join_time = time.time() - join_time
     return join_time
 
