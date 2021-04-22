5fbd99d4887f9fb7b155c199f8ed57afb0bde4c0
vibhatha <vibhatha@gmail.com>
2021-04-22 15:00:19 -0400

    update pytorch deep learning example

--- torch/pycylon_torch_example.py
+++ torch/pycylon_torch_example.py
@@ -2,10 +2,9 @@
 Install: PyCylon (Follow: https://cylondata.org/docs/)
 Run Program: mpirun -n 4 python cylon_pytorch_demo_distributed.py
 """
-import os
 import argparse
+import os
 import socket
-from datetime import timedelta
 
 import numpy as np
 import torch
@@ -13,12 +12,12 @@ import torch.distributed as dist
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from pycylon.net import MPIConfig
 from pycylon import CylonEnv
-from pycylon import DataFrame
 from pycylon import read_csv
-from torch.nn.parallel import DistributedDataParallel as DDP
+from pycylon.net import MPIConfig
 from pycylon.util.logging import log_level, disable_logging
+from sklearn.preprocessing import StandardScaler
+from torch.nn.parallel import DistributedDataParallel as DDP
 
 log_level(0)  # set an arbitrary log level
 disable_logging()  # disable logging completely
@@ -50,16 +49,12 @@ class Network(nn.Module):
         super().__init__()
 
         # Inputs to hidden layer linear transformation
-        self.hidden1 = nn.Linear(4, 1)
-        self.hidden2 = nn.Linear(1, 16)
-        self.output = nn.Linear(16, 1)
+        self.linear = nn.Linear(3, 1)
 
     def forward(self, x):
         # Pass the input tensor through each of our operations
-        x = F.relu(self.hidden1(x))
-        x = F.relu(self.hidden2(x))
-        x = self.output(x)
-        return x
+        y_pred = F.leaky_relu(self.linear(x))
+        return y_pred
 
 
 def demo_basic(rank, world_size, backend, epochs, master_address, port):
@@ -75,8 +70,8 @@ def demo_basic(rank, world_size, backend, epochs, master_address, port):
     user_devices_data = read_csv(user_devices_file, sep=',')
     user_usage_data = read_csv(user_usage_file, sep=',')
 
-    print(f"User Devices Data Rows:{len(user_devices_data)}, Columns: {len(user_devices_data.columns)}")
-    print(f"User Usage Data Rows:{len(user_usage_data)}, Columns: {len(user_usage_data.columns)}")
+    print(f"Rank [{rank}] User Devices Data Rows:{len(user_devices_data)}, Columns: {len(user_devices_data.columns)}")
+    print(f"Rank [{rank}] User Usage Data Rows:{len(user_usage_data)}, Columns: {len(user_usage_data.columns)}")
 
     print("--------------------------------")
     print("Before Join")
@@ -87,14 +82,24 @@ def demo_basic(rank, world_size, backend, epochs, master_address, port):
 
     join_df = user_devices_data.merge(right=user_usage_data, left_on=[0], right_on=[3], algorithm='sort')
     print("----------------------")
-    print("New Table After Join (5 Records)")
+    print("Rank [{}] New Table After Join (5 Records)".format(rank))
     print(join_df[0:5])
     print("----------------------")
+    feature_df = join_df[
+        ['_xplatform_version', '_youtgoing_mins_per_month', '_youtgoing_sms_per_month',
+         '_ymonthly_mb']]
+    feature_df.rename(
+        ['platform_version', 'outgoing_mins_per_month', 'outgoing_sms_per_month', 'monthly_mb'])
+    if rank == 0:
+        print("Data Engineering Complete!!!")
+    print("=" * 80)
+    print("Rank [{}] Feature DataFrame ".format(rank))
+    print(feature_df[0:5])
+    print("=" * 80)
+    data_ar: np.ndarray = feature_df.to_numpy()
 
-    data_ar: np.ndarray = join_df.to_numpy()
-
-    data_features: np.ndarray = data_ar[:, 2:6]
-    data_learner: np.ndarray = data_ar[:, 6:7]
+    data_features: np.ndarray = data_ar[:, 0:3]
+    data_learner: np.ndarray = data_ar[:, 3:4]
 
     x_train, y_train = data_features[0:100], data_learner[0:100]
     x_test, y_test = data_features[100:], data_learner[100:]
@@ -104,6 +109,13 @@ def demo_basic(rank, world_size, backend, epochs, master_address, port):
     x_test = np.asarray(x_test, dtype=np.float32)
     y_test = np.asarray(y_test, dtype=np.float32)
 
+    sc = StandardScaler()
+    sct = StandardScaler()
+    x_train = sc.fit_transform(x_train)
+    y_train = sct.fit_transform(y_train)
+    x_test = sc.fit_transform(x_test)
+    y_test = sct.fit_transform(y_test)
+
     x_train = torch.from_numpy(x_train).to(device)
     y_train = torch.from_numpy(y_train).to(device)
     x_test = torch.from_numpy(x_test).to(device)
@@ -112,11 +124,9 @@ def demo_basic(rank, world_size, backend, epochs, master_address, port):
     # create model and move it to GPU with id rank
 
     model = Network().to(device)
-
     ddp_model = DDP(model, device_ids=[device]) if cuda_available else DDP(model)
-
     loss_fn = nn.MSELoss()
-    optimizer = optim.SGD(ddp_model.parameters(), lr=0.001)
+    optimizer = optim.SGD(ddp_model.parameters(), lr=0.01)
 
     optimizer.zero_grad()
     if rank == 0:
@@ -126,10 +136,22 @@ def demo_basic(rank, world_size, backend, epochs, master_address, port):
             print(f"Epoch {t}", end='\r')
             prediction = ddp_model(x_batch)
             loss = loss_fn(prediction, y_batch)
-
-            optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+            optimizer.zero_grad()
+
+    if rank == 0:
+        print("Testing A Dummy Model")
+    prediction_model = ddp_model.train(False)
+    for x_batch, y_batch in zip(x_test, y_test):
+        prediction = prediction_model(x_batch)
+        inv_prediction = sct.inverse_transform(prediction.detach().numpy().reshape(-1, 1))
+        inv_ybatch = sct.inverse_transform(y_batch.detach().numpy().reshape(-1, 1))
+        inv_prediction = inv_prediction.reshape(1)[0]
+        inv_ybatch = inv_ybatch.reshape(1)[0]
+
+    if rank == 0:
+        print("Data Analysis Complete!!!")
 
 
 if __name__ == '__main__':
@@ -157,6 +179,6 @@ if __name__ == '__main__':
     rank = int(os.environ['OMPI_COMM_WORLD_RANK'])
     backend = args.backend
     demo_basic(rank=rank, world_size=world_size, backend=backend, epochs=args.epochs,
-                     master_address=args.master_address, port=args.port)
+               master_address=args.master_address, port=args.port)
 
     cleanup()
